00c88a471a87fd7c87947178b51bbc2fe47151c0
Gary Zhou <gzhou@records.nyc.gov>
2019-09-10 15:26:51 -0400

    Add SAML logout functionality to timeout

--- lib/auto_session_timeout_warning.rb
+++ lib/auto_session_timeout_warning.rb
@@ -10,9 +10,11 @@ module AutoSessionTimeoutWarning
       prepend_before_filter do |c|
         if c.session[:auto_session_expires_at] && c.session[:auto_session_expires_at] < Time.now
           c.send :before_timedout
+          saml_uid = session['saml_uid']
           c.send :reset_session
+          session['saml_uid'] = saml_uid
         else
-          unless c.request.original_url.start_with?(c.send(:active_url))
+          unless c.request.original_url.start_with?(c.send(:active_url, locale: nil))
             offset = seconds || (current_user.respond_to?(:auto_timeout) ? current_user.auto_timeout : nil)
             c.session[:auto_session_expires_at] = Time.now + offset if offset && offset > 0
           end
